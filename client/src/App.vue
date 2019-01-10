44f9e5b08847f0ae2bc453fbed36c805bf128815
Choi Junah <jachoi@wemb.co.kr>
2019-01-10 17:29:25 +0900

    [add] TypeScript 초기 설정 및 그리드 컴포넌트 추가

--- client/src/App.vue
+++ client/src/App.vue
@@ -4,17 +4,37 @@
     </div>
 </template>
 
-<script>
-    export default {
-        name: 'app',
-        components: {}
+<script lang="ts">
+    import Vue from 'vue';
+    import Component from "vue-class-component";
+
+    @Component
+    export default class App extends Vue {
+        name:string = 'app';
     }
 </script>
 
 <style>
-    html, body, div {
+    html, body {
+        width: 100%;
+        height: 100%;
         margin: 0;
         padding: 0;
+    }
+
+    body,
+    div,
+    ul,
+    li,
+    input,
+    textarea,
+    select,
+    option,
+    label,
+    xmp {
+        font-family: NanumSquareR;
+        text-align: left;
+        font-size: 12px;
     }
 
     #app {
--- client/src/components/board/BoardWriteComponent.vue
+++ client/src/components/board/BoardWriteComponent.vue
@@ -1,13 +1,20 @@
 <template>
-    <div>
-        Board write
-    </div>
+    <basic-grid-component></basic-grid-component>
 </template>
 
-<script>
-    export default {
-        name: "BoardWriteComponent"
+<script lang="ts">
+    import Vue from 'vue'
+    import Component from 'vue-class-component'
+    import BasicGridComponent from "../common/grid/BasicGridComponent";
+
+    @Component({
+        components: {
+            'basic-grid-component' : BasicGridComponent
+        }
+    })
+    export default class BoardWriteComponent extends Vue {
     }
+
 </script>
 
 <style scoped>
